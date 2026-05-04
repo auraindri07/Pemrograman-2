4cd82bf518974162c919d7ed649abfe0ae997e22
Auraindri <aurar4537@gmail.com>
2026-05-04 09:40:18 +0700

    fix folder PenjualanApp

---
+++ PenjualanApp/Mahasiswa.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package latconsole;
+
+/**
+ *
+ * @author Admin
+ */
+public class Mahasiswa {
+    public String nim, nama;
+    private float uts, uas;
+
+    public String getNim() {
+        return nim;
+    }
+
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public float getUts() {
+        return uts;
+    }
+
+    public void setUts(float uts) {
+        this.uts = uts;
+    }
+
+    public float getUas() {
+        return uas;
+    }
+
+    public void setUas(float uas) {
+        this.uas = uas;
+    }
+    
+}
